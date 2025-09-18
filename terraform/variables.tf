7e714847855629eaef4f186b01f1b91463702571
O-Abdelalim <omar.abdelalim@infomineo.com>
2025-09-18 17:14:21 +0300

    new cluster added

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,35 @@
+# -----------------------------------------------------------------------------
+# GCP Project & Authentication
+# -----------------------------------------------------------------------------
+
+variable "project_id" {
+  description = "The ID of the Google Cloud project where all resources will be created."
+  type        = string
+}
+
+variable "zone" {
+  description = "The GCP zone to create resources in (e.g., 'us-central1-c'). Must support A100 GPUs."
+  type        = string
+  default     = "us-central1-c"
+}
+
 variable "credentials_file_path" {
   description = "The path to the GCP credentials JSON file used to authenticate Terraform."
   type        = string
 }
+
+# -----------------------------------------------------------------------------
+# GCS Backend Bucket Variable
+# -----------------------------------------------------------------------------
 
 variable "gcs_bucket_name" {
   description = "The globally unique name for the GCS bucket for Terraform state."
   type        = string
 }
 
-variable "project_id" {
-  description = "The ID of the Google Cloud project where the service account will be created."
-  type        = string
-}
+# -----------------------------------------------------------------------------
+# Service Account Variables
+# -----------------------------------------------------------------------------
 
 variable "service_account_id" {
   description = "The desired ID for the new service account. This will be used to construct the email address of the service account."
@@ -23,13 +41,23 @@ variable "service_account_display_name" {
   type        = string
 }
 
+variable "service_account_description" {
+  description = "A descriptive text for the service account."
+  type        = string
+}
+
 variable "key_output_path" {
   description = "The local path where the service account key file will be saved."
   type        = string
   default     = "service-account-key.json"
 }
 
-variable "service_account_description" {
-  description = "A descriptive text for the service account."
+# -----------------------------------------------------------------------------
+# GKE Cluster Variable
+# -----------------------------------------------------------------------------
+
+variable "cluster_name" {
+  description = "The name for the GKE Standard cluster."
   type        = string
+  default     = "gke-a2-gpu-cluster"
 }
